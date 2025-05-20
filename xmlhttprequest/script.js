0220ee93a226216cf05837d3a31d3fe13200ba5d
Kyrylo Sheludchenko <kirill.shelydchenko@gmail.com>
2025-05-20 15:56:55 +0200

    xmlhttprequest

---
+++ xmlhttprequest/script.js
@@ -0,0 +1,22 @@
+const inputUAH = document.querySelector("#uah");
+const inputUSD = document.querySelector("#usd");
+
+inputUAH.addEventListener("input", () => {
+  const request = new XMLHttpRequest();
+
+  request.open("GET", "./current.json");
+  request.setRequestHeader("Content-type", "application/json");
+  request.send();
+
+  // "statusChanged" if requset.state === 4
+  request.addEventListener("load", () => {
+    if (request.status === 200) {
+      const data = JSON.parse(request.response);
+      inputUSD.value = (+inputUAH.value / data.current.usd).toFixed(
+        2
+      );
+    } else {
+      inputUSD.value = `Error ${request.status}`;
+    }
+  });
+});
